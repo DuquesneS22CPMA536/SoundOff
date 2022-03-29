130af2ccbebe1add006decce1ee30fc722f3e434
bsbrewer0 <61435378+bsbrewer0@users.noreply.github.com>
2022-03-29 17:23:18 -0400

    Update view_standards_window.py

--- SoundOff-seperate_files_classes/view_standards_window.py
+++ SoundOff-seperate_files_classes/view_standards_window.py
@@ -3,19 +3,20 @@ from tkinter import ttk
 from tkinter import *
 
 
-class ViewStandards(tk.Toplevel):
+class ViewPlatforms(tk.Toplevel):
     """View all available platforms and standards.
 
-    Will allow users to view every platorm with max integrated loudness (LUFS) and max peak (dB)
+    Will allow users to view every platform with max integrated loudness (LUFS) and max peak (dB)
 
     """
     def __init__(self, parent):
-        """Initializes the ViewStandards window
+        """Initializes the View Platforms window
 
-        More info if needed
+        Will display current directory of platforms by using the get_platform_names method and the standards of these
+        platforms by using the get_platform_standard method.
 
         Args:
-          self: An add new variable object
+          self: The instance of the view platforms window
           parent: App object, window it came from
 
         Raises:
@@ -24,9 +25,13 @@ class ViewStandards(tk.Toplevel):
         """
         super().__init__(parent)
         # create basic window properties
-        self.title("View Standards")
-        self.geometry("400x400")
+        self.title("View Platforms")
+        # change size according to the longest platform name
+        max_name_length = parent.get_max_platform_name_length()
+        size = str(max_name_length*6+450) + "x400"
+        self.geometry(size)
 
+        # define a scrollable frame
         container = ttk.Frame(self)
         container.pack(fill=BOTH, expand=1)
         canvas = tk.Canvas(container)
@@ -41,67 +46,87 @@ class ViewStandards(tk.Toplevel):
         )
 
         canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
-
         canvas.configure(yscrollcommand=scrollbar.set)
         canvas.configure(xscrollcommand=scrollbar.set)
 
-        style = ttk.Style()
-        style.configure(
-            "Heading.TLabel",
-            font=("Helvetica", 9, "bold")
-        )
-        style.configure(
-            "Data.TLabel",
-            font="Helvetica, 8"
-        )
-
-        standard_label = ttk.Label(
+        # define our headers
+        platform_label = ttk.Label(
             scrollable_frame,
-            text="Standard Name",
-            style="Heading.TLabel")
-
-        standard_label.grid(column=0, row=0, padx=5)
-
+            text="Platform Name",
+            style="Heading.TLabel"
+        )
         lufs_level_label = ttk.Label(
             scrollable_frame,
-            text="Max Integrated (LUFS)",
-            style="Heading.TLabel")
-
-        lufs_level_label.grid(column=1, row=0, padx=5)
+            text="Max Integrated Loudness (LUFS)",
+            style="Heading.TLabel"
+        )
         peak_level_label = ttk.Label(
             scrollable_frame,
             text="Max True Peak (dB)",
-            style="Heading.TLabel")
+            style="Heading.TLabel"
+        )
 
+        # define the look of our headers and platform data
+        style = ttk.Style()
+        style.configure(
+            "Heading.TLabel",
+            font=("Helvetica", 10, "bold")
+        )
+        style.configure(
+            "Data.TLabel",
+            font=("Helvetica", 9)
+        )
+
+        # place our headers on the screen
+        platform_label.grid(column=0, row=0, padx=5)
+        lufs_level_label.grid(column=1, row=0, padx=5)
         peak_level_label.grid(column=2, row=0, padx=5)
 
-        standard_names = parent.get_standard_names_dict()
+        platform_names = parent.get_platform_names()
         # row variable
         i = 1
-        for name in standard_names:
+        for name in platform_names:
             name_label = ttk.Label(
                 scrollable_frame,
                 text=name,
                 style="Data.TLabel"
             )
-            values = parent.get_standard_value(name)
+            values = parent.get_platform_standard(name)
+            # make sure the value is not null before trying to format it
+            if values[0] != "":
+                lufs = "{:.1f}".format(values[0])
+            else:
+                lufs = ""
+            if values[1] != "":
+                peak = "{:.1f}".format(values[1])
+            else:
+                peak = ""
 
+            # define our lufs and peak max values as labels to be placed on the screen
             lufs_label = ttk.Label(
                 scrollable_frame,
-                text=values[0],
+                text=lufs,
                 style="Data.TLabel"
             )
             peak_label = ttk.Label(
                 scrollable_frame,
-                text=values[1],
+                text=peak,
                 style="Data.TLabel"
             )
 
+            # place the platform, lufs, and peak labels on the screen
             name_label.grid(column=0, row=i, padx=5)
             lufs_label.grid(column=1, row=i, padx=5)
             peak_label.grid(column=2, row=i, padx=5)
+            # iterate the row for next platform
             i += 1
 
         container.pack()
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
+
+        # make the window modal
+        self.focus_set()
+        self.grab_set()
+        self.transient(parent)
+        self.wait_window(self)
